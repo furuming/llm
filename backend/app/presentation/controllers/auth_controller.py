7b2300b25312d6cfafa58c87680f056ffeea06f6
furumi <furumikouki@gmail.com>
2026-05-30 17:59:19 +0900

    controller, request, responseの作成

--- backend/app/presentation/controllers/auth_controller.py
+++ backend/app/presentation/controllers/auth_controller.py
@@ -1,3 +1,6 @@
+from app.presentation.schema.requests.user_request import CreateUserRequest
+from app.presentation.schema.responses.user_response import CreateUserResponse
+
 
 class AuthController:
     def __init__(self):
@@ -7,6 +10,11 @@ class AuthController:
         print("test")
         return "test"
     
-    async def create(self):
-        print("register")
-        return "register"
+    async def create(self, request: CreateUserRequest) -> CreateUserResponse:
+        print(f"register name={request.name}, email={request.email}, password={request.password}")
+
+        return CreateUserResponse(
+            name=request.name,
+            email=request.email,
+        )
+    
